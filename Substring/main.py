302c2bda701023002b498376578947d56ef6fb4f
guimo <guimolins@hotmail.com>
2018-10-01 20:33:11 +0200

    Substring exercise

--- Substring/main.py
+++ Substring/main.py
@@ -5,13 +5,10 @@ s = str(input("Please enter string to compare: "))
 for i in range(0, len(s)):
     if s[i] >= substring[len(substring)-1]:
         substring += s[i]
-
     else:
-        if len(substring) > len(longest):
-            longest = substring
-        substring = " " +s[i]
+        substring = " " + s[i]
 
-    if i == len(s)-1 and len(substring) > len(longest):
+    if len(substring) > len(longest):
         longest = substring
-
+    print(substring)
 print("Final substring:", longest[1:])
